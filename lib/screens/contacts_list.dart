481756e7e43ab6c823d92735f3cb8eafbbeca0fa
joao <joakim.vts@gmail.com>
2021-04-14 23:16:34 -0300

    Alteracoes com futureBuilder

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,5 @@
+import 'package:bytebank/database/database.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
@@ -8,25 +10,19 @@ class ContactsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Alex',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
+      body: FutureBuilder(
+        future: findAll(),
+        builder: (context, snapshot) {
+          final List<Contact> contacts = snapshot.data;
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final Contact contact = contacts[index];
+              return _ContactItem(contact);
+            },
+            itemCount: contacts.length,
           )
-        ],
+          ,
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -38,6 +34,33 @@ class ContactsList extends StatelessWidget {
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
