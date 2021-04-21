1f5285d8789b73471b3c88a57f041ae07b2cf8e3
joao <joakim.vts@gmail.com>
2021-04-20 22:58:26 -0300

    Componente de dialogo TransactionAuthDialog

---
+++ lib/componentes/transaction_auth_dialog.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+class TransactionAuthDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Authenticate'),
+      content: TextField(
+        obscureText: true,
+        maxLength: 4,
+        decoration: InputDecoration(
+            border: OutlineInputBorder()
+        ),
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+        style: TextStyle(fontSize: 64, letterSpacing: 32),
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            print('cancel');
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            print('confirm');
+          },
+          child: Text('Confirm'),
+        ),
+      ],
+    );
+  }
+}
