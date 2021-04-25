4764cee4030a07836377e56bc294dba78e5d184e
joao <joakim.vts@gmail.com>
2021-04-25 19:53:00 -0300

    http exception

--- lib/https/webclients/transaction_webclient.dart
+++ lib/https/webclients/transaction_webclient.dart
@@ -7,8 +7,7 @@ import '../webclient.dart';
 
 class TransactionWebClient {
   Future<List<Transaction>> findAll() async {
-    final Response response =
-        await client.get(baseUri);
+    final Response response = await client.get(baseUri);
     final List<dynamic> decodedJson = jsonDecode(response.body);
     return decodedJson
         .map((dynamic json) => Transaction.fromJson(json))
@@ -29,7 +28,7 @@ class TransactionWebClient {
     if (response.statusCode == 200) {
       return Transaction.fromJson(jsonDecode(response.body));
     }
-    _throwHttpError(response.statusCode);
+    throw HttpException(_statusCodeResponse[response.statusCode]);
   }
 
   static final Map<int, String> _statusCodeResponse = {
@@ -37,5 +36,9 @@ class TransactionWebClient {
     401: 'authentication failed'
   };
 
-  void _throwHttpError(int statusCode) => throw Exception(_statusCodeResponse[statusCode]);
+}
+
+class HttpException implements Exception {
+  final String message;
+  HttpException(this.message);
 }
