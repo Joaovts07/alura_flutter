10ffba30ffcc0173985720f317521320c34a9943
joao <joakim.vts@gmail.com>
2021-04-26 09:03:39 -0300

    Ajustes progress bar

--- lib/componentes/components.dart
+++ lib/componentes/components.dart
@@ -2,6 +2,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProgressBar extends StatelessWidget {
+  final String message;
+
+  const ProgressBar({this.message});
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -10,7 +14,13 @@ class ProgressBar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           CircularProgressIndicator(),
-          Text('Loading')
+          Padding(
+            padding: const EdgeInsets.only(top:8.0),
+            child: Text(
+              message,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          )
         ],
       ),
     );
